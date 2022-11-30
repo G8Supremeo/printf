8206a098e33dd0e3a7fa591012f4dacf2bb9e4b6
j-onny <omo.pro22@gmail.com>
2022-11-30 03:25:23 -0800

    next_prints

---
+++ print_str.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_str - prints a variadic string each per time to stdout
+ * @args: variadic parameter
+ *
+ * Return: number of characters printed on Success
+ * upon failure -1
+ */
+
+int print_str(va_list args)
+{
+	int count = 0;
+	char *str = va_arg(args, char *);
+
+	if (str == NULL)
+		str = "(null)";
+
+	while (*str)
+	{
+		count = _putchar(*str);
+		str++;
+	}
+
+	return (count);
+}
